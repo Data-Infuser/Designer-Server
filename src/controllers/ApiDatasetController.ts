4270dbfadba597b5beac5bc289f34f78ebe915cd
GunRo <laylow861@gmail.com>
2020-05-07 17:22:40 +0900

    datasetmanager util 분리.

--- src/controllers/ApiDatasetController.ts
+++ src/controllers/ApiDatasetController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-import { getConnection, LessThanOrEqual, getRepository, getManager } from "typeorm";
+import { getRepository } from "typeorm";
 import { Meta } from "../entity/Meta";
-
+import {DatasetManager} from "../util/DatasetManager";
 
 class ApiDatasetController {
 
@@ -12,25 +12,18 @@ class ApiDatasetController {
     try {
       console.log('api dataset show called');
 
-      let dataset = await ApiDatasetController.getDatasetByName(identifier)
+      let dataset = await DatasetManager.getDatasetByName(identifier)
       if (!dataset) {
         const meta = await metaRepo.findOne(identifier)
         if (!meta) console.log('no matching meta found')
 
-        dataset = await ApiDatasetController.getDatasetByName(meta.title)
+        dataset = await DatasetManager.getDatasetByName(meta.title)
       }
 
       res.end(dataset)
     } catch (err) {
       console.error(err);
     }
-  }
-
-  static getDatasetByName = async(name) => {
-    const dataset = await getManager()
-    .query(`SELECT * FROM ${name}`)
-
-    return JSON.stringify(dataset)
   }
 }
 
