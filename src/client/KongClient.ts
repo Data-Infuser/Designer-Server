84872f06f20ee8b23899e5897c1dc0c739029260
GunRo <laylow861@gmail.com>
2020-05-12 13:32:44 +0900

    kong url property.json 파일로 교체 .

--- src/client/KongClient.ts
+++ src/client/KongClient.ts
@@ -1,18 +1,17 @@
 import * as rm from "typed-rest-client/RestClient";
+import property from "../../property.json";
 import { KongService } from "../entity/kong/KongService";
 
-let KONG_ADMIN_BASE_URL = "http://localhost:8001";
 let KONG_SERVICE_URI = "/services";
 
 export class KongClient {
 
   static addService = async(kongService: KongService) => {
     console.log('addService called');
-    let rest: rm.RestClient = new rm.RestClient('rest-sample', KONG_ADMIN_BASE_URL);
+    let rest: rm.RestClient = new rm.RestClient('rest-sample', property.kongUrl);
 
     let restRes: rm.IRestResponse<KongService> = await rest.create<KongService>(KONG_SERVICE_URI, kongService);
     console.log('response-----');
     console.log(restRes);
-
   }
 }
