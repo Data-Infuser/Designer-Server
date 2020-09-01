d9640ad4ef67e70168b5eef9dac343fe05e53249
오충협 <tractar0213@gmail.com>
2020-09-01 14:49:34 +0900

    DB Connection test 수정

--- test/database.test.ts
+++ test/database.test.ts
@@ -1,30 +1,35 @@
 import {createConnections, getConnection} from "typeorm";
 import { expect } from "chai";
 
+const ormConfigJson = require("../ormconfig.json");
+
+const defaultConnectionInfo = ormConfigJson[0];
+const datasetConnectionInfo = ormConfigJson[1];
+
 describe('database', () => {
   it('connect test manager db', async () => {
     await createConnections([
       {
         type: "mysql",
-        host: "localhost",
-        port: 3306,
-        username: "root",
-        password: "",
-        database: "api-manager-test",
+        host: process.env.DESIGNER_HOSTNAME || defaultConnectionInfo.host,
+        port: Number(process.env.DESIGNER_PORT) || defaultConnectionInfo.port,
+        username: process.env.DESIGNER_USERNAME || defaultConnectionInfo.username,
+        password: process.env.DESIGNER_PASSWORD || defaultConnectionInfo.password,
+        database: process.env.DESIGNER_DB_NAME || defaultConnectionInfo.database,
         entities: [__dirname + "/../src/entity/manager/*.ts"],
         subscribers: [__dirname + "/../src/subscriber/**/*.ts"],
-        synchronize: true
+        synchronize: false
       },
       {
         name: "dataset",
         type: "mysql",
-        host: "localhost",
-        port: 3306,
-        username: "root",
-        password: "",
-        database: "api-dataset-test",
+        host: process.env.DESIGNER_DATASET_HOSTNAME || datasetConnectionInfo.host,
+        port: Number(process.env.DESIGNER_DATASET_PORT) || datasetConnectionInfo.port,
+        username: process.env.DESIGNER_DATASET_USERNAME || datasetConnectionInfo.username,
+        password: process.env.DESIGNER_DATASET_PASSWORD || datasetConnectionInfo.password,
+        database: process.env.DESIGNER_DATASET_DB_NAME || datasetConnectionInfo.database,
         entities: [__dirname + "/../src/entity/dataset/*.ts"],
-        synchronize: true
+        synchronize: false
       }
     ])
     const defaultConnection = getConnection();
